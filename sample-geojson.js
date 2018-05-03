fbad72b76d2e9b7331c844ac91783b991fbd93e9
Bikram Tuladhar <bikram.tuladhar@yipl.com.np>
2018-05-03 18:21:26 +0545

    Fixed the mismatched co-ordinates

--- sample-geojson.js
+++ sample-geojson.js
@@ -4,7 +4,7 @@ var radarDetails = {
         "geometry": {
             "type": "Point",
             "coordinates": [
-                7.57734989, 33.53870642, 169.22
+                -7.57734989, 33.53870642, 169.22
             ]
         },
         "type": "Feature",
@@ -16,7 +16,7 @@ var radarDetails = {
         "geometry": {
             "type": "Point",
             "coordinates": [
-                7.58738366, 33.54271929, 127.44
+                -7.58738366, 33.54271929, 127.44
             ]
         },
         "type": "Feature",
@@ -28,7 +28,7 @@ var radarDetails = {
         "geometry": {
             "type": "Point",
             "coordinates": [
-                7.56384471, 33.55397704, 141.67
+                -7.56384471, 33.55397704, 141.67
             ]
         },
         "type": "Feature",
@@ -40,7 +40,7 @@ var radarDetails = {
         "geometry": {
             "type": "Point",
             "coordinates": [
-                7.53277664, 33.56328459, 148.68
+                -7.53277664, 33.56328459, 148.68
             ]
         },
         "type": "Feature",
@@ -52,7 +52,7 @@ var radarDetails = {
         "geometry": {
             "type": "Point",
             "coordinates": [
-                7.52588983, 33.57121472, 145.5
+                -7.52588983, 33.57121472, 145.5
             ]
         },
         "type": "Feature",
@@ -64,7 +64,7 @@ var radarDetails = {
         "geometry": {
             "type": "Point",
             "coordinates": [
-                7.53397413, 33.5733531, 140.11
+                -7.53397413, 33.5733531, 140.11
             ]
         },
         "type": "Feature",
@@ -76,7 +76,7 @@ var radarDetails = {
         "geometry": {
             "type": "Point",
             "coordinates": [
-                7.51129446, 33.61270996, 70.97
+                -7.51129446, 33.61270996, 70.97
             ]
         },
         "type": "Feature",
