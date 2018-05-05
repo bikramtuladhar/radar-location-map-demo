608285521806f265dd79a8a6230f0c986e0acbb0
Bikram Tuladhar <bikramtuladhar2011@gmail.com>
2018-05-06 00:14:53 +0545

    first draft

--- sample-geojson.js
+++ sample-geojson.js
@@ -1,88 +1,86 @@
-var radarDetails = {
+eqfeed_callback({
     "type": "FeatureCollection",
     "features": [{
-        "geometry": {
-            "type": "Point",
-            "coordinates": [
-                -7.57734989, 33.53870642, 169.22
-            ]
-        },
         "type": "Feature",
         "properties": {
-            "popupContent": "Ben M'sik Boulevard Mohammed VI  Ben M'sik-sidi otmane   60  Active  le  meme sens"
+            "place": "sample location name 1",
+            "city": "sample city name 1"
         },
-        "id": 51
-    }, {
         "geometry": {
             "type": "Point",
-            "coordinates": [
-                -7.58738366, 33.54271929, 127.44
-            ]
+            "coordinates": [ -7.57734989, 33.53870642, 169.22]
         },
+        "id": "1"
+    },
+    {
         "type": "Feature",
         "properties": {
-            "popupContent": "Ben M'sik Boulevard Mohammed VI  Ben M'sik-sidi otmane   60  Active  le  meme sens"
+            "place": "sample location name 2",
+            "city": "sample city name 2"
         },
-        "id": 52
-    }, {
         "geometry": {
             "type": "Point",
-            "coordinates": [
-                -7.56384471, 33.55397704, 141.67
-            ]
+            "coordinates": [ -7.58738366, 33.54271929, 127.44]
         },
+        "id": "2"
+    },
+    {
         "type": "Feature",
         "properties": {
-            "popupContent": "Ben M'sik Boulevard Mohammed VI  Ben M'sik-sidi otmane   60  Active  le  meme sens"
+            "place": "sample location name 3",
+            "city": "sample city name 3"
         },
-        "id": 53
-    }, {
         "geometry": {
             "type": "Point",
-            "coordinates": [
-                -7.53277664, 33.56328459, 148.68
-            ]
+            "coordinates": [ -7.56384471, 33.55397704, 141.67]
         },
+        "id": "3"
+    },
+    {
         "type": "Feature",
         "properties": {
-            "popupContent": "Ben M'sik Boulevard Mohammed VI  Ben M'sik-sidi otmane   60  Active  le  meme sens"
+            "place": "sample location name 4",
+            "city": "sample city name 4"
         },
-        "id": 54
-    }, {
         "geometry": {
             "type": "Point",
-            "coordinates": [
-                -7.52588983, 33.57121472, 145.5
-            ]
+            "coordinates": [ -7.53277664, 33.56328459, 148.68]
         },
+        "id": "4"
+    },{
         "type": "Feature",
         "properties": {
-            "popupContent": "Ben M'sik Boulevard Mohammed VI  Ben M'sik-sidi otmane   60  Active  le  meme sens"
+            "place": "sample location name 5",
+            "city": "sample city name 5"
         },
-        "id": 55
-    }, {
         "geometry": {
             "type": "Point",
-            "coordinates": [
-                -7.53397413, 33.5733531, 140.11
-            ]
+            "coordinates": [ -7.52588983, 33.57121472, 145.5]
         },
+        "id": "5"
+    },
+    {
         "type": "Feature",
         "properties": {
-            "popupContent": "Ben M'sik Boulevard Mohammed VI  Ben M'sik-sidi otmane   60  Active  le  meme sens"
+            "place": "sample location name 6",
+            "city": "sample city name 6"
         },
-        "id": 56
-    }, {
         "geometry": {
             "type": "Point",
-            "coordinates": [
-                -7.51129446, 33.61270996, 70.97
-            ]
+            "coordinates": [ -7.53397413, 33.5733531, 140.11]
         },
+        "id": "6"
+    },
+    {
         "type": "Feature",
         "properties": {
-            "popupContent": "Ben M'sik Boulevard Mohammed VI  Ben M'sik-sidi otmane   60  Active  le  meme sens"
+            "place": "sample location name 7",
+            "city": "sample city name 7"
         },
-        "id": 57
+        "geometry": {
+            "type": "Point",
+            "coordinates": [ -7.51129446, 33.61270996, 70.97]
+        },
+        "id": "6"
     }]
-};
+});
